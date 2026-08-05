563ee75125a79774325c9559ef5c5cf6241db90e
Shivam Kumar Patel <shivamsinghvns100@gmail.com>
2026-08-05 23:30:57 +0530

    i learn today bubble sort

---
+++ sorting/src/BubbleSort.java
@@ -0,0 +1,27 @@
+public class BubbleSort {
+    public static void bubbleSort(int[] arr){
+        int n= arr.length;
+        int temp;
+        for (int i = 0; i < n-1; i++) {
+            int flag=0;
+            for (int j = 0; j < n-1-i; j++) {
+                if(arr[j]>arr[j+1]){
+                    temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                    flag=1;
+                }
+            }
+            if(flag==0)
+                break;
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr={15,16,6,8,5};
+        BubbleSort.bubbleSort(arr);
+        System.out.println("array after sorting");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(" "+arr[i]);
+        }
+    }
+}
