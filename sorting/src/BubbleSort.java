3afdd1b16d2a53b2b8bdf1353f11bcca2f84a8f3
Shivam Kumar Patel <shivamsinghvns100@gmail.com>
2026-08-05 23:46:26 +0530

    update data type of flag variable from int to boolean with variable swapped

--- sorting/src/BubbleSort.java
+++ sorting/src/BubbleSort.java
@@ -3,16 +3,16 @@ public class BubbleSort {
         int n= arr.length;
         int temp;
         for (int i = 0; i < n-1; i++) {
-            int flag=0;
+            boolean swapped=false;
             for (int j = 0; j < n-1-i; j++) {
                 if(arr[j]>arr[j+1]){
                     temp=arr[j];
                     arr[j]=arr[j+1];
                     arr[j+1]=temp;
-                    flag=1;
+                    swapped=true;
                 }
             }
-            if(flag==0)
+            if(swapped==false)
                 break;
         }
     }
